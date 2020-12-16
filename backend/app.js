6eedfca2f6e9a474b377e934bed2826fb6a340c1
Roman Zavalii <romasty_92@mail.ru>
2020-12-16 23:55:38 +0300

    cors add

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cors({
-  credentials: true,
-  origin: true,
-}));
+app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.json());
